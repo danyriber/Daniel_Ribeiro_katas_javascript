d175e7b86c9ef01c293ec6485c7622c75e7d483c
Daniel <danyriber3232@gmail.com>
2026-01-15 07:02:14 +0100

    ejercicios2

---
+++ Ejercicios_dom/Ejercicio1/script.js
@@ -0,0 +1,18 @@
+const boton = document.querySelector('.showme');
+console.log(boton);
+
+const cabecero = document.querySelector('#pillado');
+console.log(cabecero);
+
+
+const parrafos = document.querySelectorAll('p');
+console.log(parrafos);
+
+const bichos = document.querySelectorAll('.pokemon');
+console.log(bichos);
+
+const elementos = document.querySelectorAll('[data-function="testMe"]');
+console.log(elementos);
+
+const personajes = document.querySelectorAll('[data-function="testMe"]');
+console.log(personajes[2]);
