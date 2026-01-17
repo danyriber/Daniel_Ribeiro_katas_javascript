445bf5534d3ef853eee79f094a7c59b2cf181c96
Daniel <danyriber3232@gmail.com>
2026-01-17 11:27:04 +0100

    katas finalizadas

---
+++ Ejercicios_dom/Ejercicio5/script.js
@@ -0,0 +1,16 @@
+
+const albums = [
+  "De Mysteriis Dom Sathanas",
+  "Reign of Blood",
+  "Ride the Lightning",
+  "Painkiller",
+  "Iron Fist",
+];
+const app = document.querySelector('#albums');
+const ul = document.createElement('ul');
+    albums.forEach(album => {
+      const li = document.createElement('li');
+      li.textContent = album;
+      ul.appendChild(li);
+    })
+app.appendChild(ul);
